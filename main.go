00440ff8a12a43107d962c6f8f2d44fd2fc94be5
k-k-0 <aadityyadav7531@gmail.com>
2025-10-12 16:00:55 +0530

    Added multi user connected and send bytes each other

--- main.go
+++ main.go
@@ -3,16 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
 
 var wsUpgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+	// ReadBufferSize:  1024,
+	// WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
+
+var clients []*websocket.Conn
+var mutex sync.Mutex
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := wsUpgrader.Upgrade(w, r, nil)
@@ -23,17 +27,41 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 	defer conn.Close()
 
+	mutex.Lock()
+	clients = append(clients, conn)
+	mutex.Unlock()
+
+	log.Println("client connected")
+
 	for {
-		messageType, msg, err := conn.ReadMessage()
+		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("error while reading message", err)
 			break
 		}
-		if err := conn.WriteMessage(messageType, msg); err != nil {
-			log.Println("error while writing", err)
+
+		log.Println("received:", string(msg))
+
+		mutex.Lock()
+		for _, c := range clients {
+			if c != conn {
+				err := c.WriteMessage(websocket.TextMessage, msg)
+				if err != nil {
+					log.Println("Write Error", err)
+				}
+			}
+		}
+		mutex.Unlock()
+	}
+
+	mutex.Lock()
+	for i, c := range clients {
+		if c == conn {
+			clients = append(clients[:i], clients[i+1:]...)
 			break
 		}
 	}
+	mutex.Unlock()
 }
 
 func main() {
